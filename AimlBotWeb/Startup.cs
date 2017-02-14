32ad6a0e656f70ff9b49db453e479906bc970318
Ivan Taturevich <ivan_taturevich@epam.com>
2017-02-14 12:19:23 +0300

    initialize commit

---
+++ AimlBotWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AimlBotWeb.Startup))]
+namespace AimlBotWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
